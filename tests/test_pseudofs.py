e926926b51408853e50916527547faa81bcb4c0b
Guy Wilson <guywilsonjr@gmail.com>
2024-12-12 00:19:33 -0800

    Benchmarking working

--- tests/test_pseudofs.py
+++ tests/test_pseudofs.py
@@ -1,8 +1,19 @@
 import asyncio
 import logging
-
+from typing import Coroutine
 from std_utils.pseudofs import cache
+import pytest
+from pytest_benchmark.plugin import benchmark
 
-def test_cache():
-    logging.info(cache.get_cpu_list())
-    logging.info(asyncio.run(cache.get_cache_cpu_info('cpu0')))
+
+
+
+@pytest.mark.benchmark
+def test_cache(benchmark):
+
+    @benchmark
+    def run():
+        return asyncio.run(cache.get_all_cpu_info())
+
+    assert run
+
