0f77bfea81e7d1ce5e66299cdac4fa2cbce045ac
Guy Wilson <guywilsonjr@gmail.com>
2024-12-12 00:35:45 -0800

    Minor modifications

--- tests/test_pseudofs.py
+++ tests/test_pseudofs.py
@@ -1,19 +1,21 @@
 import asyncio
 import logging
 from typing import Coroutine
+
+from _pytest.mark import ParameterSet
+
 from std_utils.pseudofs import cache
 import pytest
 from pytest_benchmark.plugin import benchmark
 
 
 
-
+@pytest.mark.parametrize(('async_func',), ((cache.get_all_cpu_info,),))
 @pytest.mark.benchmark
-def test_cache(benchmark):
-
+def test_async_func(benchmark, async_func: Coroutine):
     @benchmark
-    def run():
-        return asyncio.run(cache.get_all_cpu_info())
+    def sync_test_func():
+        return asyncio.run(async_func())
 
-    assert run
+    assert sync_test_func
 
